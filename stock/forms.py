33fb47d8ff8902e5468d9bcd084a7a54cab1af54
Gustavo Costa <gustavo@meucredere.com.br>
2025-03-25 13:44:55 -0300

    Refatoração da pagina de edição de produto

--- stock/forms.py
+++ stock/forms.py
@@ -5,8 +5,20 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for visible in self.visible_fields():
-            visible.field.widget.attrs['class'] = 'form-control'
+            if visible.field.__class__.__name__ in ['ModelChoiceField']:
+                visible.field.widget.attrs['class'] = 'form-select'
+            else:
+                visible.field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = Product
         fields = ["name", "bar_code", "manufacturer", "manufacturer_code", "unit_type", "amount", "unit_price"]
+        labels = {
+            "bar_code": "Código de barras",
+            "name": "Nome",
+            "manufacturer": "Fabricante",
+            "manufacturer_code": "Código do fabricante",
+            "unit_type": "Tipo de unidade",
+            "amount": "Quantidade",
+            "unit_price": "Preço unitário",
+        }
